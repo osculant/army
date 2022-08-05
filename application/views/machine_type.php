3ec0c00be9e594049d7aa90191dcb9ed9c96e181
Aman Rastogi <rastogiaman1998@gmail.com>
2022-08-05 18:31:30 +0530

    changes

--- application/views/machine_type.php
+++ application/views/machine_type.php
@@ -6,6 +6,8 @@
       <meta name="apple-mobile-web-app-capable" content="yes">
       <meta name="apple-mobile-web-app-status-bar-style" content="#3a57c4">
       <title>DEWS</title>
+
+     
       <!-- <link rel="shortcut icon" type="image/x-icon" href="https://inspirestory.co.in/amazing-fact/assets/img/favicon.png"> -->
       <link rel="stylesheet" href="<?php echo base_url('assets/attachment') ?>/bootstrap.min.css">
       <link rel="stylesheet" href="<?php echo base_url('assets/attachment') ?>/fontawesome.min.css">
@@ -24,7 +26,59 @@
       <script type="text/javascript" async="" src="<?php echo base_url('asets/loader/') ?>ga.js.download"></script>
       <script src="<?php echo base_url('asets/loader/') ?>snap.svg-min.js.download"></script>
 <style type="text/css">
-   
+       .lds-ripple {
+    display: inline-block;
+    position: absolute;
+    width: 80px;
+    height: 80px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 9999;
+    background: transparent;
+}
+.lds-ripple div {
+  position: absolute;
+  border: 4px solid teal;
+  opacity: 1;
+  border-radius: 50%;
+  animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+}
+.lds-ripple div:nth-child(2) {
+  animation-delay: -0.5s;
+}
+@keyframes lds-ripple {
+  0% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
+  4.9% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
+  5% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 1;
+  }
+  100% {
+    top: 0px;
+    left: 0px;
+    width: 72px;
+    height: 72px;
+    opacity: 0;
+  }
+}
+
+
 
 
       /*------------overlay for header--------------*/
@@ -90,6 +144,11 @@
       
    </head>
    <body >
+
+<div class="lds-ripple" id="loader"><div></div><div></div></div>
+
+<div id="main_id" style="display: none;">
+
       <div class="main-wrapper">
           <div class="navbar two-action no-hairline" style="background-color: black;">
             <div class="navbar-inner d-flex align-items-center">
@@ -106,26 +165,24 @@
 
       <!-- header overlay -->
 
-<div id="myNav" class="overlay">
-  <button class="btn btn-link closebtn" style="background-color: transparent;" onclick="closeNav()"><i class="fa fa-arrow-left"></i></button>
-  <div class="overlay-content">
-  <!-- <hr style="margin-top: 0rem;"> -->
+        <div id="myNav" class="overlay">
+                
+                <button class="btn btn-link closebtn" style="background-color: transparent;" onclick="closeNav()"><i class="fa fa-arrow-left"></i></button>
+                  <div class="overlay-content">
+                  <!-- <hr style="margin-top: 0rem;"> -->
 
-    
-    <a href="<?php echo base_url('index.php') ?>">Home</a>
-    <a href="<?php echo base_url('index.php/contact_us') ?>">Contact US</a>
-    <a href="<?php echo base_url('index.php/about_us') ?>">About Us</a>
-    <!-- <a href="https://www.tripleodiagnostics.com">Visit Our Website</a> -->
-
-    <!-- <a href="<?php echo base_url('index.php/login/logout') ?>">Logout</a> -->
-    
-    
-  </div>
+                    
+                    <a href="<?php echo base_url('index.php') ?>">Home</a>
+                    <a href="<?php echo base_url('index.php/contact_us') ?>">Contact US</a>
+                    <a href="<?php echo base_url('index.php/about_us') ?>">About Us</a>
+                  
+                    
+                  </div>
    
-  </div>
+        </div>
 
 
-</div>
+        
 <!-- overlay end -->
 
 
@@ -159,13 +216,10 @@
             </div>
          </div>
       </div>
-       <!-- <div class="footer-cont">
-         <ul class="ul-footers">
-            <li><a href="notifications.html" class="icon-only" style="cursor: pointer;"><i class="fa fa-share-alt" aria-hidden="true"></i></a></li>
-            <li><a onclick="myFunction()" href="#" class="link icon-only darsjht"><i class="fa fa-adjust" aria-hidden="true"></i></a></li>
-            <li><a class="link icon-only darsjht"><i class="fa fa-share"></i></a></li>
-         </ul>
-          </div> -->
+      
+
+
+      </div>   <!-- main div  -->
                    
        
        <script src="<?php echo base_url('assets/attachment') ?>/jquery-3.5.1.min.js.download"></script>
@@ -178,10 +232,7 @@
        <script src="<?php echo base_url('assets/loader/') ?>svgLoader.js.download"></script>
        <script>
          
-function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-}
+
 </script>
        
 
@@ -198,6 +249,13 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 
 }
+
+
+window.onload = function(){  
+    
+    setTimeout(function(){ $('#loader').hide();  $('#main_id').show();   }, 2000);
+
+}  
 
 </script>
 
