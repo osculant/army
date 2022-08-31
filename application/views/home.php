de48e3eefa80a4ef8cae7a017ffe8de14feedd63
Aman Rastogi <rastogiaman1998@gmail.com>
2022-09-01 00:22:10 +0530

    changes

--- application/views/home.php
+++ application/views/home.php
@@ -4,27 +4,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">  
   <title>ORASCAN</title>
 
-      <style>
-         
-         @media(max-height: 1024px){
-
-      body{
-        height: 100vh;
-      }
-
-    }
-         
-        
-
-        @media(max-height: 700px){
-          body{
-            height: 130vh;
-          }
-        }
+      
      
-     
-      </style>
-      <!--links-->
+     <!--links-->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
@@ -47,49 +29,47 @@
       <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.js" integrity="sha512-ZKNVEa7gi0Dz4Rq9jXcySgcPiK+5f01CqW+ZoKLLKr9VMXuCsw3RjWiv8ZpIOa0hxO79np7Ec8DDWALM0bDOaQ==" crossorigin="anonymous"></script>
       <!--script end-->
 </head>
-<body style="    background: linear-gradient(to bottom, #1fcb42 0%, #e8f3d8 100%);">
+<body style="    background: linear-gradient(to bottom, #1fcb42 0%, #e8f3d8 100%);height: 100vh;overflow-x: hidden;">
 
-  <div class="container">
+  <div class="container" >
     
-    <div class="row d-flex justify-content-center">
+    <div class="row d-flex justify-content-center" style="margin-top: 28%;">
       <div class="first">
-        <img src="<?php echo base_url('assets/logo.png') ?>" style="width: 87px;margin-top: 58px;">
+        <img src="<?php echo base_url('assets/logo.png') ?>" style="width: 87px;">
       </div>
-
     </div>
 
 
-    <div class="row" style="margin-top: 9px;">
+    <div class="row" style="">
       <div class="col-12">
-      <div class="tag"><h4 style="color: white; text-align: center;text-shadow: 2px 2px 4px black;">DHRUVA ECO-FRIENDLY</h4>
-        <p style="color: white; text-align: center;text-shadow: 2px 2px 4px black;">WASTE DISPOSAL SYSTEM</p>
+        <div class="tag">
+          <h4 style="color: white; text-align: center;text-shadow: 2px 2px 4px black;">DHRUVA ECO-FRIENDLY</h4>
+          <p style="color: white; text-align: center;text-shadow: 2px 2px 4px black;">WASTE DISPOSAL SYSTEM</p>
+        </div>
       </div>
 
-
-      <div class="cover_image">
-        <center><img src="<?php echo base_url('assets/middle.png') ?>" style="width: 21rem;margin-top: 22px;"></center>
+      <div class="col-12">
+        <div class="cover_image">
+          <center><img src="<?php echo base_url('assets/middle.png') ?>" style="width: 21rem;margin-top: 22px;"></center>
+        </div>
       </div>
 
 
     
       <div class="col-12">
-      <div class="buttons">
-       
-         <a href="<?php echo base_url('index.php/welcome/content2') ?>"><button class="btn btn-info" style="background-color: transparent;width: -webkit-fill-available;margin-top: 50px;color: #17a2b8;border:2px solid #17a2b8 ;border-radius: 15px;margin-bottom: 30px;">Continue <i class="fa fa-arrow-circle-right"></i></button></a>
-        
+        <div class="buttons">
+          <a href="<?php echo base_url('index.php/welcome/content2') ?>"><button class="btn btn-info" style="background-color: transparent;width: -webkit-fill-available;margin-top: 50px;color: #17a2b8;border:2px solid #17a2b8 ;border-radius: 15px;margin-bottom: 30px;">Continue <i class="fa fa-arrow-circle-right"></i></button></a>
+        </div>
       </div>
-      </div>
-
-
 
     </div>
+  
+  
   </div>
 
 
 
 
-
-  </div>
 
 </body>
 <script type="text/javascript">
