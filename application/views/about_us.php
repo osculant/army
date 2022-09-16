1dab9c47a13565522732c7c9d0d49baafd112365
Aman Rastogi <rastogiaman1998@gmail.com>
2022-09-17 00:32:58 +0530

    changes

--- application/views/about_us.php
+++ application/views/about_us.php
@@ -5,42 +5,211 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, minimal-ui, viewport-fit=cover">
       <meta name="apple-mobile-web-app-capable" content="yes">
       <meta name="apple-mobile-web-app-status-bar-style" content="#3a57c4">
-      <title>Demo</title>
-      <link rel="shortcut icon" type="image/x-icon" href="https://inspirestory.co.in/amazing-fact/assets/img/favicon.png">
+      <title>Detail</title>
       <link rel="stylesheet" href="<?php echo base_url('assets/attachment') ?>/bootstrap.min.css">
       <link rel="stylesheet" href="<?php echo base_url('assets/attachment') ?>/fontawesome.min.css">
       <link rel="stylesheet" href="<?php echo base_url('assets/attachment') ?>/all.min.css">
       <link rel="stylesheet" href="<?php echo base_url('assets/attachment') ?>/swiper.min.css">
       <link rel="stylesheet" href="<?php echo base_url('assets/attachment') ?>/circle.css">
       <link rel="stylesheet" href="<?php echo base_url('assets/attachment') ?>/style.css">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+      <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap&family=Pacifico&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+      <link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'>
+      <link href="https://fonts.googleapis.com/css2?family=Cormorant+SC&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Bilbo+Swash+Caps&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;500&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Rubik+Moonrocks&display=swap" rel="stylesheet">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;500&display=swap" rel="stylesheet">
+
+<!-- <link href="https://fonts.googleapis.com/css2?family=Combo&display=swap" rel="stylesheet"> -->
+
+<link href="https://fonts.googleapis.com/css2?family=Combo&family=Volkhov&display=swap" rel="stylesheet">
+      <!--links end-->
+
+      <!--script-->
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+      <script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
+      <script src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.js" integrity="sha512-ZKNVEa7gi0Dz4Rq9jXcySgcPiK+5f01CqW+ZoKLLKr9VMXuCsw3RjWiv8ZpIOa0hxO79np7Ec8DDWALM0bDOaQ==" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.js" integrity="sha512-ZKNVEa7gi0Dz4Rq9jXcySgcPiK+5f01CqW+ZoKLLKr9VMXuCsw3RjWiv8ZpIOa0hxO79np7Ec8DDWALM0bDOaQ==" crossorigin="anonymous"></script>
+      <!--script end-->
+
+<style type="text/css">
+   
+   body {
+  background-color:black;
+  color: white;
+}
+
+
+
+      /*------------overlay for header--------------*/
+
+      .circle{
+        width: 16rem;
+    height: 16rem;
+    background: #77D6B4;
+    position: absolute;
+    right: -48px;
+    border-radius: 50%;
+    top: -39px;
+      }
+
+
+       .circle2{
+          width: 11rem;
+    height: 11rem;
+    background: #64ab92;
+    position: absolute;
+    left: -23px;
+    border-radius: 50%;
+    bottom: -63px;
+      }
+      .overlay {
+            opacity: 0.97;
+          height: 100%;
+          width: 0;
+          position: fixed;
+          z-index: 100;
+          top: 0;
+          left: 0;
+          background-color: black;
+          
+          overflow-x: hidden;
+          transition: 0.5s;
+           box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+              /* border-top: 6px solid brown;*/
+        }
+
+        .overlay-content {
+             position: relative;
+            top: 22%;
+            width: 100%;
+            text-align: center;
+        }
+
+
+        .overlay a {
+            padding: 8px;
+            text-decoration: none;
+            font-size: 18px;
+            color: white;
+            display: block;
+            transition: 0.3s;
+            /*border: 1px solid white;*/
+            margin: 10px;
+            /*border-radius: 6px;*/
+            text-transform: uppercase;
+            font-weight: 200;
+            box-shadow: 0 0 0px 0 rgb(0 0 0 / 10%), 0 0px 8px 0 rgb(0 0 0 / 19%);
+            margin-top: 4px;
+            text-align: initial;
+            
+            
+        }
+
+
+
+        .overlay a:hover, .overlay a:focus {
+          color: #f1f1f1;
+        }
+
+        .overlay .closebtn {
+          position: absolute;
+            top: 21px;
+            font-size: 20px;
+            right: 14px;
+        }
+
+        .navigation{
+         margin-left: 22px;
+         font-size: 0.5rem;
+        }
+
+      /*------------overlay for header end-----------*/
+</style>
    </head>
    <body style="height:100vh">
+     
+
       <div class="main-wrapper">
-          <div class="navbar two-action no-hairline">
-            <div class="navbar-inner d-flex align-items-center" style="height: 60px;">
-               <p><a href="javascript:history.back()" ><img src="<?php echo base_url('assets/5626618.png') ?>" style="width: 25px;    position: absolute;
-    top: 15px;"></a></p>
-               <div class="sliding custom-title" style="text-align: center;">About Us</div>
+          <div class="navbar two-action no-hairline" style="background-color: black;">
+            <div class="navbar-inner d-flex align-items-center">
+               
+               <div class="sliding custom-title" style="text-align: left;margin-top: 2px;">
+                   
+
+
+                <h5 style="color: #9FE0CE;font-family: 'Rubik Moonrocks', cursive;margin-top: 28px;margin-left: 0px;text-align: left;">DHRUVA ECO-FRIENDLY</h5>
+                <p><span class="arc" style="color:#9FE0CE;text-align:left;font-family: 'Rubik Moonrocks', cursive;">  WASTE DISPOSAL SYSTEM</span></p>
+             
+
+               </div>
                <div class="right d-flex">
                   
+                  <p><i onclick="openNav()" class="fa fa-bars" style="color:gray;margin-top: 16px;margin-left: 77px;font-size: 1.5rem;"></i></p>
+               
                </div>
             </div>
          </div>
       </div>
-      <div class="page-content">
-         <div class="social-media-col pages-list-col">
-            <div class="container">
-            
+
+
+      <!-- header overlay -->
+
+<div id="myNav" class="overlay">
+    <div class="circle"></div>
+    <div class="circle2"></div>
+  <button class="btn btn-link closebtn" style="background-color: transparent;" onclick="closeNav()"><i class="fa fa-close"></i></button>
+  <div class="overlay-content">
+  
+    
+   <?php include "common/sidebar.php"; ?>
+    
+    
+  </div>
+   
+  </div>
+
+
+</div>
+<!-- overlay end -->
+
+       
+         <div class="navigation" style="display:flex">
+
+
+            <p><a href="<?php echo base_url(); ?>" style="text-decoration: none;color: white;">Home </a>  / 
+                About us
+            </p>
+      
          </div>
-      </div>
-       <!-- <div class="footer-cont">
-         <ul class="ul-footers">
-            <li><a href="notifications.html" class="icon-only" style="cursor: pointer;"><i class="fa fa-share-alt" aria-hidden="true"></i></a></li>
-            <li><a onclick="myFunction()" href="#" class="link icon-only darsjht"><i class="fa fa-adjust" aria-hidden="true"></i></a></li>
-            <li><a class="link icon-only darsjht"><i class="fa fa-share"></i></a></li>
-         </ul>
-          </div> -->
-                   
+
+
+      <div class="page-content " >
+
+
+         <div class="social-media-col pages-list-col">
+            <div class="container animate__animated animate__bounceInRight" style="transition: 0.5s;">
+                  
+
+               <center>
+                  <h5 style="color:white;">ABOUT US</h5>
+               </center>
+
+            </div>
+         </div>
+       </div>            
        
        <script src="<?php echo base_url('assets/attachment') ?>/jquery-3.5.1.min.js.download"></script>
        <script src="<?php echo base_url('assets/attachment') ?>/popper.min.js.download"></script>
@@ -49,9 +218,15 @@
        <script src="<?php echo base_url('assets/attachment') ?>/script.js.download"></script>
        <script>
          
-function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
+   function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+  
+
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+
 }
 </script>
        
---
+++ application/views/common/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+<center><p>
+	Powered By <a href="https://osculant.in/">Osculant Techonologies</a>
+</p></center>
+</div>
